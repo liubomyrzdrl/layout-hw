30cff05ea9b06d9538e6550ba8ff0701ac9c1393
MacBook <macbook@MacBook-Pro-MacBook.local>
2021-05-21 14:42:21 +0300

    add key  to array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useLayout } from "./utils/useLayout";
+import { v4 as uuid } from "uuid"
 import "./App.css";
 
 function App() {
   const { columns } = useLayout();
-  const blocksOfLayout = Array(6).fill().map(() => <div className="block"></div>)
+  const blocksOfLayout = Array(6).fill().map(() => <div key={uuid()} className="block"></div>)
+
   return (
     <div className="App">
       <div className={`layout layout-col${columns}`}>
